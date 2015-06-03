d35072cab0daa73ece7bc26a70c6b07dcc61f0c3
jaspreet_home <jaspreet.channa@gmail.com>
2015-06-03 23:00:59 +1000

    added partials

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/**
+ * Created by jaspreet on 3/06/15.
+ */
+
+
+var app = angular.module('spa',['ngRoute', 'ngAnimate']);
+
+
+app.config(function($routeProvider){
+ $routeProvider
+          .when('/',{
+                templateUrl: 'partials/home.html'
+          })
+          .when('/page1',{
+                templateUrl: 'partials/page1.html'
+          })
+        .when('/page2',{
+                        templateUrl: 'partials/page2.html'
+                  })
+      .when('/page3',{
+                      templateUrl: 'partials/page3.html'
+                })
+                    .when('/page4',{
+                                    templateUrl: 'partials/page4.html'
+                              });
+});
+
+
+app.controller('mainController',['$scope', function($scope) {
+$scope.test = 'Hola!';
+}])
